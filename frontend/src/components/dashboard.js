db25547a163491edea894a0dd35c8928e2ecbc79
rahul <mrahulreddy2902@gmail.com>
2022-04-13 06:13:24 +0100

    [dashboard]: Added dashboard class and updated the import statements.

--- frontend/src/components/dashboard.js
+++ frontend/src/components/dashboard.js
@@ -1,26 +1,34 @@
 import React from "react";
 import "./dashboard.css";
-import { Col, Form, Row, Container, Dropdown } from "react-bootstrap";
+import {
+  Col,
+  Form,
+  Row,
+  Container,
+  Dropdown,
+  DropdownButton,
+  ButtonGroup,
+} from "react-bootstrap";
 
 const dashboard = () => {
   return (
     <>
-      <div className="dashboardContainer" id="dashboardContainer">
-        <Form>
-          <div className="dropdown">
-            <Dropdown>
-              <Dropdown.Toggle variant="success" id="dropdown-basic">
-                Parking Place 1
-              </Dropdown.Toggle>
+      <div className="container">
+        <label for="cars">Choose Place:</label>
 
-              <Dropdown.Menu>
-                <Dropdown.Item href="#/Place-1">Place-1</Dropdown.Item>
-                <Dropdown.Item href="#/Place-2">Place-2 </Dropdown.Item>
-                <Dropdown.Item href="#/Place-3">Place-3 </Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown>
-          </div>
-        </Form>
+        <select name="places" id="places">
+          <option value="Abbey Wood">Abbey Wood		</option>
+          <option value="Acton">Acton			</option>
+          <option value="Acton Green">Acton Green		</option>
+          <option value="Addington">Addington		</option>
+		  <option value="Addiscombe">Addiscombe		</option>
+		  <option value="Aldborough Hatch ">Aldborough Hatch </option>
+		  <option value="Aldersbrook">Aldersbrook		</option>
+		  <option value="Alperton">Alperton	        </option>
+		  <option value="Anerley">Anerley			</option>
+		  
+		  
+        </select>
       </div>
     </>
   );
