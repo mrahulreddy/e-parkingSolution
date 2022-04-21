5f9b21af498cba6ca518716543e4582b3062396e
rahul <mrahulreddy2902@gmail.com>
2022-04-21 11:44:29 +0100

    [function-update]: added usenavigate instead of usehistory since we have upgraded version.

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -2,24 +2,26 @@ import { Button, Container, Form } from "react-bootstrap";
 import { useEffect, useState } from "react";
 import axios from "axios";
 //import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 import Loading from "./loading";
 import ErrorMessage from "./ErrorMessage";
-const Login = ({ history }) => {
+const Login = () => {
   const [email, setEmail] = useState("");
   const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+  let navigate = useNavigate();
+  useEffect(() => {
+    const userInfo = localStorage.getItem("userInfo");
 
-  // useEffect(() => {
-  //   const userInfo = localStorage.getItem("userInfo");
-
-  //   console.log(userInfo);
-  //   if (userInfo) {
-  //     history.push("/dashboard");
-  //   }
-  // }, [history]);
+    console.log(userInfo);
+    if (userInfo) {
+      navigate("/dashboard");
+      console.log("hi rahul");
+    }
+  }, [navigate]);
 
   const signInSubmitHandler = async (e) => {
     e.preventDefault();
@@ -44,10 +46,11 @@ const Login = ({ history }) => {
 
       localStorage.setItem("userInfo", JSON.stringify(data));
       setLoading(false);
+      navigate("/dashboard");
       // console.log("pass");
       // console.log(history);
       // history.push("/dashboard");
-      window.location = "http://localhost:3000/dashboard";
+      // window.location = "http://localhost:3000/dashboard";
     } catch (error) {
       setLoading(false);
       setError(error);
