78cc1950b541dca6e13c132f8beab9bda8b5ff52
rahul <mrahulreddy2902@gmail.com>
2022-05-14 00:49:17 +0100

    [frontend]: Connection added for addAdmin and addOwner

--- frontend/src/components/addAdmin.js
+++ frontend/src/components/addAdmin.js
@@ -10,36 +10,87 @@ import {
   Button,
 } from "react-bootstrap";
 import axios from "axios";
+import SucessMessage from "./SucessMessage";
 
 const AddAdmin = () => {
   const [udata, setUdata] = useState([]);
+  const [suser, setSuser] = useState([]);
+  const [sucess, setSucess] = useState(false);
   const get_users_data = async () => {
     const { data } = await axios.get("/api/users/getusers");
-    // console.log("inside data");
-    // console.log(data);
     setUdata(data);
   };
 
+  const addAsAdmin = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      var email = suser;
+
+      const { data } = await axios.put(
+        "/api/users/addadmin",
+        {
+          email,
+        },
+        config
+      );
+      setSucess("Sucessfully Added as Admin");
+    } catch (error) {}
+  };
+
+  const addAsOwner = async (e) => {
+    e.preventDefault();
+    try {
+      const config = {
+        headers: {
+          "Content-type": "application/json",
+        },
+      };
+      var email = suser;
+
+      const { data } = await axios.put(
+        "/api/users/addowner",
+        {
+          email,
+        },
+        config
+      );
+      setSucess("Sucessfully Added as Owner");
+    } catch (error) {}
+  };
+
   useEffect(() => {
+    setSucess(false);
     get_users_data();
+
+    document
+      .getElementById("user-options")
+      .addEventListener("change", function () {
+        setSuser(this.value);
+      });
   }, []);
 
   return (
     <div>
       <Container>
         <h2> Add Admin :</h2>
+        {sucess && <SucessMessage message={sucess} />}
         <Form.Label>List of users:</Form.Label>
-        <select>
+        <select id="user-options">
           {udata.map((dat) => (
             <option> {dat.email} </option>
           ))}
         </select>
         <br />
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={addAsAdmin}>
           Add as admin
         </Button>
         &nbsp;&nbsp;
-        <Button variant="primary" type="submit">
+        <Button variant="primary" onClick={addAsOwner}>
           Add as Land Owner
         </Button>
       </Container>
